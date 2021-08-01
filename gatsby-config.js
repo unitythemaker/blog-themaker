167c9ab2f520f79c09702d55e0ce6901b161fa5b
Halil Tezcan KARABULUT <83316880+unitythemaker@users.noreply.github.com>
2021-08-02 02:12:03 +0300

    Configuration attempt for GAnalytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "G-S218SFH6LL",
+        trackingId: "UA-281390787-1",
         head: true,
       }
     },
